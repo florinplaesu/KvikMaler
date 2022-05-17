bd1f344c19df91984589b66d6a20530695c76024
florinplaesu <81465876+florinplaesu@users.noreply.github.com>
2022-05-17 22:00:27 +0200

    even smaller changes

--- app/src/main/java/com/example/KvikMaler2/MainActivity.java
+++ app/src/main/java/com/example/KvikMaler2/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "A message should be sent pressing this", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
